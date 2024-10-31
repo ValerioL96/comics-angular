25f837508e93c442c90c2c3e5c1a3f10894ed66a
ValerioL96 <v.lembo1396@gmail.com>
2024-10-31 12:59:00 +0100

    Fixed Header and added main

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+  navBarComics = [
+    {
+      image: '/img/buy-comics-digital-comics.png',
+      text: 'CHARACTERS'
+    },
+    {
+      image: '/img/buy-comics-merchandise.png',
+      text: 'COMICS'
+    },
+    {
+      image: '/img/buy-comics-shop-locator.png',
+      text: 'MOVIES'
+    },
+    {
+      image: '/img/buy-comics-subscriptions.png',
+      text: 'TV'
+    },
+    {
+      image: '/img/buy-dc-power-visa.svg',
+      text: 'GAMES'
+    }
+  ];
+}
